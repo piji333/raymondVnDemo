53bc017a8acc45e1ae602e4582e77af5a040ae8a
Phuoc <phamhbphuoc@gmail.com>
2024-08-27 00:52:39 +0900

    temp2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://piji333.github.io',
-    // base: 'raymondVnDemo',
+    base: '/raymondVnDemo',
 });
