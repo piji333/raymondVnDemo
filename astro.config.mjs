c5a1837dbe324c89323df5084f7f59eb5c595639
Phuoc <phamhbphuoc@gmail.com>
2024-08-28 02:17:07 +0900

    images path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://piji333.github.io',
+  site: 'https://piji333.github.io/raymondVnDemo',
   // base: '',
   integrations: [relativeLinks()]
 });
