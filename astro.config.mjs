4c22b160e6203f242cbdcdcd1922976725cb2878
Phuoc <phamhbphuoc@gmail.com>
2024-08-28 01:23:14 +0900

    relative link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://piji333.github.io/raymondVnDemo',
-    base: '/raymondVnDemo',
+  site: 'https://piji333.github.io/raymondVnDemo',
+  base: '/raymondVnDemo',
+  integrations: [relativeLinks()]
 });
