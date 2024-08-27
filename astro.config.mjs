06a3e297f22dde6301b5bb7f2602759bd06fbd80
Phuoc <phamhbphuoc@gmail.com>
2024-08-28 00:27:30 +0900

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://piji333.github.io',
+    site: 'https://piji333.github.io/raymondVnDemo',
     base: '/raymondVnDemo',
 });
