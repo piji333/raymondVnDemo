2bbb9d36ca8e8fd0dc4cc48aa4ce4c10487d7808
Phuoc <phamhbphuoc@gmail.com>
2024-09-08 23:57:08 +0900

    changed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://piji333.github.io/raymondVnDemo',
-  // base: '',
+  // site: 'https://piji333.github.io/raymondVnDemo',
+  site: 'https://piji333.github.io',
+  base: '/raymondVnDemo',
   integrations: [relativeLinks()]
 });
