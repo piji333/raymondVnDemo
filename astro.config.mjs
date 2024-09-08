feda5d4e2335b700f7ee4721bb96801728ebc87d
Phuoc <phamhbphuoc@gmail.com>
2024-09-09 00:42:01 +0900

    set baseUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://piji333.github.io/raymondVnDemo',
   site: 'https://piji333.github.io',
   base: '/raymondVnDemo',
   integrations: [relativeLinks()]
